35ebfde6584cd476f22845bbf5f0b69c2a4d4ac5
Michael Rittenhouse <monkey0506@gmail.com>
2024-10-06 15:31:12 -0500

    Remove old debugging code

--- ImplementationClassCollection.Key.cs
+++ ImplementationClassCollection.Key.cs
@@ -21,26 +21,11 @@ namespace Monkeymoto.NativeGenericDelegates
             {
                 MethodReference = methodReference;
                 hashCode = Hash.Combine(MethodReference.Method, MethodReference.Marshalling);
-                Debug.WriteLine($"hashCode: {hashCode}");
             }
 
             public override bool Equals(object? obj) => obj is Key other && Equals(other);
             public bool Equals(Key other) => (MethodReference.Method == other.MethodReference.Method) &&
                 (MethodReference.Marshalling == other.MethodReference.Marshalling);
-            //public bool Equals(Key other)
-            //{
-            //    Debug.WriteLine($"interface: {MethodReference.Method.ContainingInterface.FullName} / {other.MethodReference.Method.ContainingInterface.FullName} / equal? {MethodReference.Method.ContainingInterface == other.MethodReference.Method.ContainingInterface}");
-            //    Debug.WriteLine($"method: {MethodReference.Method.Name} / {other.MethodReference.Method.Name} / equal? {MethodReference.Method == other.MethodReference.Method}");
-            //    Debug.WriteLine($"arity: {MethodReference.Method.Arity} / {other.MethodReference.Method.Arity}");
-            //    Debug.WriteLine($"aritys equal? {MethodReference.Method.Arity == other.MethodReference.Method.Arity}");
-            //    Debug.WriteLine($"interfaces equal? {MethodReference.Method.ContainingInterface == other.MethodReference.Method.ContainingInterface}");
-            //    Debug.WriteLine($"names equal? {MethodReference.Method.Name == other.MethodReference.Method.Name}");
-            //    Debug.WriteLine($"methods equal? {MethodReference.Method == other.MethodReference.Method}");
-            //    //public bool Equals(MethodDescriptor? other) =>
-            //    //    (other is not null) && (Arity == other.Arity) && (ContainingInterface == other.ContainingInterface) &&
-            //    //    (Name == other.Name);
-            //    return (MethodReference.Method == other.MethodReference.Method) && (MethodReference.Marshalling == other.MethodReference.Marshalling);
-            //}
             public override int GetHashCode() => hashCode;
         }
     }
